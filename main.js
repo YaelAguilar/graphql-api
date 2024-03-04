80f4f636653e4539c75c59761de57439c972ba98
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-03-04 02:52:46 -0600

    [ADD] graphql route

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('server');
+const express = require('express')
+const {graphqlHTTP} = require('express-graphql')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('ruta inicial')
+})
+
+app.use('/graphql', graphqlHTTP({
+    //schema,
+    graphiql: true
+}))
+
+app.listen(3000)
+console.log('server is running on port 3000')
