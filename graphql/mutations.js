df4325e2a2f7136823bb9414484baa8826d0b231
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-03-04 09:56:00 -0600

    [EDIT] login & register  mutations

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -1,34 +1,36 @@
-const { GraphQLString } = require("graphql");
+const { GraphQLString, GraphQLID, GraphQLNonNull } = require('graphql');
 const { User } = require('../models');
 const { createJWTToken } = require('../util/auth');
 
 const register = {
     type: GraphQLString,
-        description: 'Registra un usuario y retorna un token',
+        description: 'Registrar usuarios',
         args: {
-            username: { type:GraphQLString },
-            email: { type:GraphQLString },
-            password: { type:GraphQLString },
-            displayName: { type:GraphQLString },
+            username: { type: new GraphQLNonNull(GraphQLString) },
+            email: { type: new GraphQLNonNull(GraphQLString) },
+            password: { type: new GraphQLNonNull(GraphQLString) },
+            displayName: { type: new GraphQLNonNull(GraphQLString) },
         },
-        async resolve(_, args) {
-            const { username, email, password, displayName} = args;
-            const user = new User({username, email, password, displayName})
-                await user.save();
-
-                const token = createJWTToken(user);
-
-                console.log(user);
-                console.log(token);
-                return token;
-        },
+        async resolve(_, { username, email, password, displayName }) {
+            const user = new User({ username, email, password, displayName });
+            user.password = await bcrypt.encryptPassword(user.password);
+            await user.save();
+        
+            const token = auth.createJWTToken({
+              _id: user._id,
+              email: user.email,
+              displayName: user.displayName,
+            });
+            return token;
+          },
 };
 
 const login = {
     type: GraphQLString,
+    description: 'Iniciar Sesion',
     args: {
-        email: { type:GraphQLString },
-        password: { type:GraphQLString },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        password: { type: new GraphQLNonNull(GraphQLString) },
     },
     async resolve(_, { email, password }) {
         const user = await User.findOne({ email }).select("+password");
