9f02bb841de1060843f57abedf514fd0b3479ef5
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-03-04 09:37:40 -0600

    [ADD] auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,18 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
 
 const authenticate = (req, res, next) => {
-
-    const token = req.headers.autorization?.split(" ")[1];
-    console.log(token)
-    next()
+  const token = req.headers.authorization?.split(" ")[1] || "";
+  try {
+    const verified = jwt.verify(token, JWT_SECRET);
+    req.verifiedUser = verified.user;
+    next();
+  } catch (error) {
+    // console.error("error:", error);
+    next();
+  }
 };
 
 module.exports = {
-    authenticate
+  authenticate,
 };
