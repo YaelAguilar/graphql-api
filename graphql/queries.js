928346563ac75694865babd0e216df1d53a4aaf1
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-03-05 02:31:03 -0600

    [EDIT] posts query has been update to include pagination

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,4 +1,4 @@
-const { GraphQLList, GraphQLID, GraphQLNonNull } = require('graphql');
+const { GraphQLList, GraphQLID, GraphQLNonNull, GraphQLInt } = require('graphql');
 const { UserType, PostType, CommentType } = require('./types');
 const { User, Post, Comment } = require('../models');
 
@@ -19,14 +19,35 @@ const user = {
     resolve: (_, { id }) => User.findById(id),
   };
   
-//query para obtener lista de posts (3)  
+//query para obtener lista de posts (paginacion 1)  
+const posts = {
+  type: new GraphQLList(PostType),
+  description: "Recupera una lista paginada de posts",
+  args: {
+    first: {
+      type: GraphQLInt,
+      description: 'Número de posts a recuperar en la página.',
+    },
+    offset: {
+      type: GraphQLInt,
+      description: 'Número de posts a saltar desde el inicio.',
+    },
+  },
+  resolve: (_, { first = 10, offset = 0 }) => {
+    return Post.find().skip(offset).limit(first);
+  },
+
+/*
+query para obtener lista de posts (sin paginacion)
 const posts = {
     type: new GraphQLList(PostType),
     description: "Recupera una lista de posts",
     resolve: () => Post.find(),
   };
+*/  
+};
 
-//query para obtener un posts (4)  
+//query para obtener un post (3)  
   const post = {
     type: PostType,
     description: "Recupera un solo post",
@@ -34,14 +55,14 @@ const posts = {
     resolve: (_, { id }) => Post.findById(id),
   };  
 
-//query para obtener lista de comentarios (5)
+//query para obtener lista de comentarios (4)
 const comments = {
   type: new GraphQLList(CommentType),
   description: "Recuperada lista de comentarios",
   resolve: () => Comment.find(),
 };  
 
-//query para obtener un comentario (6)
+//query para obtener un comentario (5)
 const comment = {
   type: CommentType,
   description: "Recuperado un comentario",
